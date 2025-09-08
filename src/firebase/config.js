64088168db915481e04bb44cf6a29d4e9c76b529
Manuel Sayago <manuelsayago15@gmail.com>
2025-09-08 15:20:37 -0300

    Env config and firebase variables, remove hard-coded keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,20 +3,21 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTfMrb-s9qCXWVLspMzVROlU2irbkeOA4",
-  authDomain: "sayahalloween-9e2fd.firebaseapp.com",
-  projectId: "sayahalloween-9e2fd",
-  storageBucket: "sayahalloween-9e2fd.firebasestorage.app",
-  messagingSenderId: "1076115604252",
-  appId: "1:1076115604252:web:bb85ec2eb9ad7370e9f0d9",
-  measurementId: "G-80MPJGESXV"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app)
 
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 export { db }
