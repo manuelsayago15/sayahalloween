c46b88909945dcea26e42bcd37e04e5c07cb93c1
Manuel Sayago <manuelsayago15@gmail.com>
2025-10-16 21:25:42 -0300

    Confirming party's date text

--- src/pages/components/HeroSection.jsx
+++ src/pages/components/HeroSection.jsx
@@ -29,8 +29,7 @@ const HeroSection = () => {
             <div className="overlay"></div>
                 <div className="hero-content">
                     <h1>La fiesta de disfraces más épica del año 🎃👻</h1>
-                    {/* <h2>Viernes 31 de Octubre</h2> */}
-                    <h2>Fecha por confirmar</h2>
+                    <h2>Viernes 31 de Octubre</h2>
                     <h2>7:00 PM</h2>
                     
                     <Countdown />
